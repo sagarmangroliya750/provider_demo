1de66109c9b75b44bf46770e7ff5b5f12749c207
Sagar Mangroliya <sagarspatel750@gmail.com>
2023-04-06 14:38:31 +0530

    d

--- lib/myclass.dart
+++ lib/myclass.dart
@@ -5,7 +5,7 @@ class myclass extends ChangeNotifier
   int sum=0;
   void total(String a, String b)
   {
-    sum=int.parse(a)+int.parse(b);
+    sum = int.parse(a)+int.parse(b);
     notifyListeners();
   }
 }
--- lib/providerdemo.dart
+++ lib/providerdemo.dart
@@ -1,4 +1,6 @@
 
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'myclass.dart';
@@ -25,43 +27,47 @@ class _providerdemoState extends State<providerdemo> {
       body: Column(
         children: [
           Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.all(10),
             child: TextField(controller: t1,keyboardType:TextInputType.number,
-            decoration:InputDecoration(border:OutlineInputBorder(borderRadius:BorderRadius.circular(20)),
+            decoration:InputDecoration(border:OutlineInputBorder(borderRadius:BorderRadius.circular(30)),
               hintText:"Enter First Value",suffixIcon:IconButton(onPressed: () {
                 setState(() {
                   t1.text = "";
                   m.sum = 0;
                 });
-              }, icon:Icon(Icons.clear,color:Colors.red,))
+              }, icon:Icon(Icons.clear,color:Colors.red))
             )),
           ),
           Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.all(10),
             child: TextField(controller: t2,keyboardType:TextInputType.number,
-            decoration:InputDecoration(border:OutlineInputBorder(borderRadius:BorderRadius.circular(20)),
+            decoration:InputDecoration(border:OutlineInputBorder(borderRadius:BorderRadius.circular(30)),
                 hintText:"Enter Second Value",suffixIcon:IconButton(onPressed: () {
                  setState(() {
                    t2.text = "";
                    m.sum = 0;
                  });
-                }, icon:Icon(Icons.clear,color:Colors.red,))
+                }, icon:Icon(Icons.clear,color:Colors.red))
             ),
             ),
+          ),
+          Container(
+            height:40,width:150,
+            alignment:Alignment.center,
+            margin:EdgeInsets.only(top:30,bottom:30),
+            decoration:BoxDecoration(borderRadius:BorderRadius.circular(5),border:Border.all(color:Colors.green)),
+            child: Text("Sum = ${m.sum}",style:TextStyle(fontSize:20,color:Colors.green)),
           ),
           ElevatedButton(style: ElevatedButton.styleFrom(
-            shape:RoundedRectangleBorder(borderRadius:BorderRadius.circular(15)),elevation:4
+            shape:RoundedRectangleBorder(borderRadius:BorderRadius.circular(15)),elevation:4,
+            fixedSize:Size(150, 40)
           ),
               onPressed: () {
-            String a=t1.text;
-            String b=t2.text;
+            String a = t1.text;
+            String b = t2.text;
             m.total(a,b);
 
-          }, child: Text("Ans Of Sum")),
-          Container(
-            margin:EdgeInsets.only(top:30),
-            child: Text("Sum = ${m.sum}",style:TextStyle(fontSize:17),),
-          )
+          }, child: Text("Answer Of Sum",style:TextStyle(letterSpacing:1.5),)),
         ],
       ),
     );
